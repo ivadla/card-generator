ed9aa4a55bb6023d7a542de1347001f72151eccd
ivadla <ivadla@arcor.de>
2011-10-26 13:44:25 +0200

    fix treasure-reaction and victory-reaction cards

--- src/CardCompositorFactory.java
+++ src/CardCompositorFactory.java
@@ -2,9 +2,9 @@ public class CardCompositorFactory {
 
 	public static CardCompositor create(CardParameters card,
 			CardTemplate template) {
-		if (card.getTypeTemplate().equals("action-victory") || card.getTypeTemplate().equals("basic-victory")) {
+		if (card.getTypeTemplate().equals("action-victory") || card.getTypeTemplate().equals("basic-victory") || card.getTypeTemplate().equals("victory-reaction")) {
 			return new CardCompositorVictory(template);
-		} else if (card.getTypeTemplate().equals("treasure") || card.getTypeTemplate().equals("basic-treasure")) {
+		} else if (card.getTypeTemplate().equals("treasure") || card.getTypeTemplate().equals("basic-treasure") || card.getTypeTemplate().equals("treasure-reaction")) {
 			return new CardCompositorTreasure(template);
 		} else if (card.getTypeTemplate().equals("treasure-victory")) {
 			return new CardCompositorTreasureVictory(template);
